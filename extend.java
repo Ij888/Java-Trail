a3fae2394b72b27dc3abfcfdd26111630f9b8e97
Ijem <ij.ofili@gmail.com>
2015-07-26 17:19:25 +0100

    added file

---
+++ extend.java
@@ -0,0 +1,5 @@
+class MountainBike extends Bicycle{ //new class "MountainBike" inherits properties from parent "Bicycle" class 
+    public static void Main(String[]args){
+        //new fields and methods for defining a mountain bike goes here
+    }
+}
