d2af3818557ffef460668677a6fd71b06d7b3c90
Ij888 <ij.ofili@gmail.com>
2015-07-28 22:59:45 +0100

    corrected errors

--- className.java
+++ className.java
@@ -2,9 +2,9 @@ class ClassName{
 	public static void main(String[]args){
 		for(j=0; j<5; j++){
 			if(j%2 == 0 ){
-				System.out.println('we\'re even')
+				System.out.println('we\'re even');
 			}else{
-				System.out.println('j is so odd')
+				System.out.println('j is so odd');
 			}
 			}
 		}
