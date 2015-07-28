e6fbfeba28c42ad9f0f280502bbad0fba280b0df
Ij888 <ij.ofili@gmail.com>
2015-07-28 22:21:53 +0100

    added file
    trying out i8n

---
+++ className.java
@@ -0,0 +1,12 @@
+class ClassName{
+	public static void main(String[]args){
+		for(j=0; j<5; j++){
+			if(j%2 == 0 ){
+				System.out.println('we\'re even')
+			}else{
+				System.out.println('j is so odd')
+			}
+			}
+		}
+	}
+}
