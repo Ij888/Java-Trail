8878723eb35c5b73deefc44c4b02fcc9e30d3d4e
Ij888 <ij.ofili@gmail.com>
2015-07-30 20:39:56 +0100

    nth day kata

---
+++ arithmetics.java
@@ -0,0 +1,9 @@
+class Arithmetic{
+    public static void main(String[]main){
+        int sum1, sum2, sum;
+        sum1 = 3;
+        sum2 = 5;
+        sum = sum1 + sum2;
+        System.out.println(sum);
+    }
+}
