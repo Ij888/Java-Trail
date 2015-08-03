0190bf618193ff1d03b70e5d724c3f0138248ea9
Ijem <ij.ofili@gmail.com>
2015-08-03 22:47:33 +0100

    added entry

---
+++ Gray.java
@@ -0,0 +1,12 @@
+class Gray {
+    public void speak() {
+        System.out.println("fraise");
+    }
+}
+
+	public static void main(String[] args) {
+		Robot sam = new Robot();
+            sam.speak();
+		
+	}
+}
