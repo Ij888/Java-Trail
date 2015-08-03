75f473d2e4910c104affcf8b5bba175e4fd7a628
Ijem <ij.ofili@gmail.com>
2015-08-03 20:22:23 +0100

    added files
    experimenting on own capabilities with the Java methods

---
+++ Weapons.java
@@ -0,0 +1,13 @@
+class Weapons{
+    String name = firearm;
+    int rounds = 500;
+    
+    void shoot(){
+        System.out.println("kapoww!")
+    }
+}
+
+public class App{
+    public static void main(String[]args){
+        Weapon mark5 = new Weapon();
+    }
