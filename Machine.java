917ff45f577f572dc7a0ff67d8919a06331564ab
Ij888 <ij.ofili@gmail.com>
2015-08-03 19:51:01 +0100

    added file
    constructors in Java

---
+++ Machine.java
@@ -0,0 +1,10 @@
+class Machine{
+    public Machine(){
+        system.out.printn("10001");    
+    }
+    
+    public static void main(String[]args){
+        Machine machine1 = new Machine();
+    }
+    
+}
