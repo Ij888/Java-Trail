88ec3857d2e6125bfe03948f3634136c40bb8694
Ijem <ij.ofili@gmail.com>
2015-08-04 23:10:14 +0100

    modified entry

--- Weapons.java
+++ Weapons.java
@@ -2,12 +2,12 @@ class Weapons{
     //String name;// = "firearm";
     //int rounds;// = 500;
     
-    public void shoot(){
+     void shoot(){
         System.out.println("kapoww!");
     }
 }
 
- class App{
+ public class App{
     public static void main(String[] args){
         Weapons mark5 = new Weapons();
         mark5.shoot();
